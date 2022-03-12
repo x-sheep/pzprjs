f062d8184a8342ac40490ca951d4bc4a7dfa0986
Lennard Sprong <x-sheep@users.noreply.github.com>
2022-03-12 13:42:58 +0100

    Add Rassi Silai

--- src/pzpr/variety.js
+++ src/pzpr/variety.js
@@ -70,7 +70,8 @@
 		"cbanana",
 		"oneroom",
 		"bdwalk",
-		"voxas"
+		"voxas",
+		"rassi"
 	]);
 
 	(function(Genre, obj) {
@@ -291,6 +292,7 @@
 				"pipelink"
 			],
 			putteria: [0, 0, "プッテリア", "Putteria", "hanare"],
+			rassi: [0, 0, "Rassi Silai", "Rassi Silai", "country"],
 			rectslider: [0, 0, "四角スライダー", "Rectangle-Slider", "bonsan"],
 			reflect: [1, 0, "リフレクトリンク", "Reflect Link"],
 			renban: [0, 0, "連番窓口", "Renban-Madoguchi"],
