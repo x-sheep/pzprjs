c03bceacce55138f4429667e716d52ded3d71409
Lennard Sprong <x-sheep@users.noreply.github.com>
2022-05-02 12:04:18 +0200

    Add placeholder example puzzle

--- test/script/statuepark.js
+++ test/script/statuepark.js
@@ -1,7 +1,14 @@
 /* statuepark.js */
 
 ui.debug.addDebugData("statuepark", {
-	url: "1/1/0",
-	failcheck: [[null, "pzprv3/statuepark/1/1/"]],
+	url: "7/7/dghnmggmdgpehgmhi//t",
+	// TODO add proper example
+	failcheck: [
+		[
+			null,
+			"pzprv3/statuepark/7/7/t/1 1 1 1 2 1 1 /2 2 2 1 2 2 1 /1 1 2 1 1 2 1 /2 1 1 1 1 1 1 /2 1 2 2 1 1 1 /2 1 2 2 1 2 1 /2 1 1 1 2 2 2 /. . . . # . . /# # # . # # . /. . # . . # . /# . . . . . . /# . # # . . . /# . # # . # . /# . . . # # # /"
+		]
+	],
+	// TODO add input tests
 	inputs: []
 });
