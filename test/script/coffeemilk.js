767ed018116cf45729d73074dc42c14fdfe681c5
Lennard Sprong <x-sheep@users.noreply.github.com>
2022-06-01 11:01:15 +0200

    Add example puzzle

---
+++ test/script/coffeemilk.js
@@ -0,0 +1,12 @@
+/* coffeemilk.js */
+
+ui.debug.addDebugData("coffeemilk", {
+	url: "4/4/11a2d.a2a21a./",
+	failcheck: [
+		[
+			null,
+			"pzprv3/coffeemilk/4/4/1 1 . 2 /. . . . /- . 2 . /2 1 . - /1 0 0 /0 0 0 /1 1 0 /0 1 1 /1 0 0 1 /1 0 0 1 /1 0 0 1 /"
+		]
+	],
+	inputs: []
+});
