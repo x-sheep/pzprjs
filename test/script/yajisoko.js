644603371500b3e007cef9358a83f74411032f6f
Lennard Sprong <x-sheep@users.noreply.github.com>
2022-08-12 21:23:52 +0200

    Add more input tests

--- test/script/yajisoko.js
+++ test/script/yajisoko.js
@@ -76,6 +76,26 @@ ui.debug.addDebugData("yajisoko", {
 			label: "Arrow with keyboard",
 			input: ["newboard,2,2", "key,down,1,shift+right"],
 			result: "pzprv3/yajisoko/2/2/. . /4,1 . /0 /0 /0 0 /0 0 /0 0 /"
+		},
+		{
+			label: "Boxes input mode",
+			input: ["newboard,2,2", "editmode,box", "mouse,left,1,1,3,1,3,3,1,3"],
+			result: "pzprv3/yajisoko/2/2/# # /# # /0 /0 /0 0 /0 0 /0 0 /"
+		},
+		{
+			label: "Play inputs",
+			input: [
+				"newboard,4,1",
+				"editmode",
+				"cursor,1,1",
+				"key,q",
+				"playmode",
+				"mouse,left,1,1,3,1",
+				"mouse,left,1,1",
+				"mouse,left,3,1",
+				"mouse,right,5,1,7,1"
+			],
+			result: "pzprv3/yajisoko/1/4/# . . . /1 0 0 /17 0 2 2 /"
 		}
 	]
 });
