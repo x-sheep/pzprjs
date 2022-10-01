8c4d4b1c10b270a0b68b9e70bd75a10a867a8615
Lennard Sprong <x-sheep@users.noreply.github.com>
2022-10-01 12:34:51 +0200

    WIP of input tests

--- test/script/anglers.js
+++ test/script/anglers.js
@@ -49,5 +49,23 @@ ui.debug.addDebugData("anglers", {
 			"pzprv3/anglers/5/5/. 8 . . 5 . . /. . . # . . . /. . . 0 . . . /. . . . . 0 . /2 . . . . . . /. 0 0 . 0 . 3 /. . . -2 . . . /0 1 0 0 1 0 /0 1 0 1 1 0 /0 1 0 1 0 0 /1 0 0 0 0 0 /0 0 0 0 0 1 /1 0 0 1 0 /0 1 0 0 1 /1 0 0 0 0 /0 1 1 1 1 /1 1 1 1 1 /0 0 1 0 0 /"
 		]
 	],
-	inputs: []
+	inputs: [
+		{
+			label: "Click to input fish in cell",
+			input: ["newboard,2,2", "editmode", "mouse,leftx3,3,3"],
+			result:
+				"pzprv3/anglers/2/2/. . . . /. . . . /. . 0 . /. . . . /0 0 0 /0 0 0 /0 0 /0 0 /0 0 /"
+		},
+		{
+			label: "Click to input number in excell",
+			input: ["mouse,leftx3,3,-1"],
+			result:
+				"pzprv3/anglers/2/2/. . 1 . /. . . . /. . 0 . /. . . . /0 0 0 /0 0 0 /0 0 /0 0 /0 0 /"
+		},
+		{ label: "Type number in cell" },
+		{ label: "Type shaded cell" },
+		{ label: "Draw from excell" },
+		{ label: "Prevent draw from invalid excell" },
+		{ label: "Prevent draw through shade" }
+	]
 });
