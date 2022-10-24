7b019aefbcc99fc0a244deba794248257febb098
Lennard Sprong <x-sheep@users.noreply.github.com>
2022-10-24 14:03:28 +0200

    Add sample puzzle

---
+++ test/script/mirrorbk.js
@@ -0,0 +1,12 @@
+/* mirrorbk.js */
+
+ui.debug.addDebugData("mirrorbk", {
+	url: "4/4/133k2m0400c0",
+	failcheck: [
+		[
+			null,
+			"pzprv3/mirrorbk/4/4/1 3 3 . /. . . . /2 . . . /. . . . /1 1 2 /2 1 1 /1 0 1 /1 1 2 /1 2 1 2 /1 1 0 0 /2 1 1 2 /"
+		]
+	],
+	inputs: []
+});
