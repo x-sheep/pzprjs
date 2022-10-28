0b39ceb46c0753cdc43fe0509a64589d3f0e62ef
Lennard Sprong <x-sheep@users.noreply.github.com>
2022-10-28 18:09:59 +0200

    Add inputtests

--- test/script/mirrorbk.js
+++ test/script/mirrorbk.js
@@ -34,5 +34,15 @@ ui.debug.addDebugData("mirrorbk", {
 			"pzprv3/mirrorbk/4/4/1 3 3 . /. . . . /2 . . . /. . . . /1 1 2 /2 1 1 /1 0 1 /1 1 2 /1 2 1 2 /1 1 0 0 /2 1 1 2 /"
 		]
 	],
-	inputs: []
+	inputs: [
+		{ input: ["newboard,2,2", "editmode"] },
+		{
+			input: ["mouse,rightx2,3,3", "mouse,left,2,0,2,2"],
+			result: "pzprv3/mirrorbk/2/2/. . /. 4 /0 /2 /2 2 /"
+		},
+		{
+			input: ["playmode", "mouse,left,2,2,2,4"],
+			result: "pzprv3/mirrorbk/2/2/. . /. 4 /0 /1 /2 2 /"
+		}
+	]
 });
