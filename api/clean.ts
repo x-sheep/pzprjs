a5c2481cab3ccfb4fe094d9bea104aa972ca8a32
Lennard Sprong <x-sheep@users.noreply.github.com>
2022-11-13 18:31:06 +0100

    WIP of redirect server

---
+++ api/clean.ts
@@ -0,0 +1,18 @@
+import { VercelRequest, VercelResponse } from '@vercel/node';
+
+function decode_vercel_query(url: string): string {
+	const decoded = decodeURIComponent(url);
+	return decoded.replace(/\s/g, "+");
+}
+
+export default (request: VercelRequest, response: VercelResponse) => {
+	const tokens = request.url?.split('?') ?? "";
+	if(tokens.length === 1) {
+		response.redirect(308, '/p');
+		return;
+	}
+	
+	// Using Vercel's default redirect will corrupt the query string. Use a server function to redirect manually
+	const qs = decode_vercel_query(tokens[1]);
+	response.redirect(308, '/p?' + qs);
+}
