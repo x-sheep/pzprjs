8314aee14e8dcb564c177b307eabc5d6d3228543
Lennard Sprong <x-sheep@users.noreply.github.com>
2022-11-15 20:49:41 +0100

    Test

--- api/clean.ts
+++ api/clean.ts
@@ -7,12 +7,9 @@ function decode_vercel_query(url: string): string {
 
 export default (request: VercelRequest, response: VercelResponse) => {
 	const tokens = request.url?.split('?') ?? "";
-	if(tokens.length === 1) {
-		response.redirect(308, '/p');
-		return;
-	}
 	
 	// Using Vercel's default redirect will corrupt the query string. Use a server function to redirect manually
 	const qs = decode_vercel_query(tokens[1]);
-	response.redirect(308, '/p?' + qs);
+	response.end(`${request.url}\n${qs}`);
+	// response.redirect(308, '/p?' + qs);
 }
