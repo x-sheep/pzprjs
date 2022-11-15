fcae63bf685b84e03004a0c4d563bb5a4b812f61
Lennard Sprong <x-sheep@users.noreply.github.com>
2022-11-15 21:03:44 +0100

    Fix

--- api/clean.ts
+++ api/clean.ts
@@ -1,15 +1,19 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
+import { VercelRequest, VercelResponse } from "@vercel/node";
 
 function decode_vercel_query(url: string): string {
-	const decoded = decodeURIComponent(url);
-	return decoded.replace(/\s/g, "+");
+  const decoded = decodeURIComponent(url);
+  return decoded.replace(/\s/g, "+");
 }
 
 export default (request: VercelRequest, response: VercelResponse) => {
-	const tokens = request.url?.split('?') ?? "";
-	
-	// Using Vercel's default redirect will corrupt the query string. Use a server function to redirect manually
-	const qs = decode_vercel_query(tokens[1]);
-	response.end(`${request.url}\n${qs}`);
-	// response.redirect(308, '/p?' + qs);
-}
+  const tokens = request.url?.split("?") ?? [""];
+  const base = tokens[0];
+
+  if (tokens.length === 1) {
+    response.redirect(`https://puzz.link${base}`);
+  } else {
+    // Using Vercel's default redirect will corrupt the query string. Use a server function to redirect manually
+    const qs = decode_vercel_query(tokens[1]);
+    response.redirect(`https://puzz.link${base}?${qs}`);
+  }
+};
