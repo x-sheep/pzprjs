3e80ad1ec8733f1ad560015165df4477aac344a3
Lennard Sprong <x-sheep@users.noreply.github.com>
2026-03-26 09:30:10 +0100

    Add example

---
+++ test/script/roboticwalk.js
@@ -0,0 +1,12 @@
+/* roboticwalk.js */
+
+ui.debug.addDebugData("roboticwalk", {
+	url: "5/5/6604c6q13q",
+	failcheck: [
+		[
+			null,
+			"pzprv3/roboticwalk/5/5/6 . # # . /. . # # . /. . 1 3 . /. . # . . /. # # . . /0 1 1 1 0 0 /0 0 1 -1 0 0 /0 1 0 0 0 0 /0 0 0 0 0 0 /0 0 0 1 0 0 /0 0 0 0 0 /1 0 0 1 0 /1 1 1 1 0 /0 0 1 1 0 /0 0 1 1 0 /0 0 0 0 0 /"
+		]
+	],
+	inputs: []
+});
