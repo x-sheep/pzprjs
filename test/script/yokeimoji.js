445ec246bcd09165cfbac47ee5b17695b3d7bde8
Lennard Sprong <x-sheep@users.noreply.github.com>
2026-04-27 19:01:43 +0200

    Improve example

--- test/script/yokeimoji.js
+++ test/script/yokeimoji.js
@@ -1,10 +1,38 @@
 /* yokeimoji.js */
 ui.debug.addDebugData("yokeimoji", {
-	url: "4/4/-2b-2b..-54d-43-2b-54-43-54-2bd-54d./",
+	url: "4/4/-62-62..-1cd-23-62-1c-23-1c-62d-1cd./",
 	failcheck: [
 		[
+			"brNoShade",
+			"pzprv3/yokeimoji/4/4/リリーー/インカリ/イカイリ/ンインー/. . . . /. . . . /. . . . /. . . . /"
+		],
+		[
+			"csAdjacent",
+			"pzprv3/yokeimoji/4/4/リリーー/インカリ/イカイリ/ンインー/# . . # /. . # . /. . . . /. # # . /"
+		],
+		[
+			"cuDivideRB",
+			"pzprv3/yokeimoji/4/4/リリーー/インカリ/イカイリ/ンインー/. . . # /. . # . /. # . . /# . . . /"
+		],
+		[
+			"cuBadStart",
+			"pzprv3/yokeimoji/4/4/リリーー/インカリ/イカイリ/ンインー/. . . # /. . . . /. . . . /# . . # /"
+		],
+		[
+			"cuBadStart",
+			"pzprv3/yokeimoji/4/4/リリーー/インカリ/イカイリ/ンインー/# . . # /. . # . /. . . . /. . # . /"
+		],
+		[
+			"cuBadSequence",
+			"pzprv3/yokeimoji/4/4/リリーー/インカリ/イカイリ/ンインー/. . . # /. . # . /. . . . /# . . . /"
+		],
+		[
+			"bankGt",
+			"pzprv3/yokeimoji/4/4/リリーー/インカリ/イカイリ/ンインー/. . . # /# . # . /. . . . /# . # . /"
+		],
+		[
 			null,
-			"pzprv3/yokeimoji/4/4/ココーー/ボンニコ/ボニボコ/ンボンー/# + + # /+ + # + /+ + + + /# + # + /"
+			"pzprv3/yokeimoji/4/4/リリーー/インカリ/イカイリ/ンインー/# + + # /+ + # + /+ + + + /# + # + /"
 		]
 	],
 	inputs: [
